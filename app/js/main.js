602fcad3697669ef2ac1ce205d8e9230fa902ef2
NightMareStories <73705208+NightMareStories@users.noreply.github.com>
2020-11-21 18:50:29 +0300

    tenth day

--- app/js/main.js
+++ app/js/main.js
@@ -6,26 +6,47 @@ document.querySelector('.icon-menu').addEventListener('click', function (e) {
 })
 
 let in_burger = document.querySelector('.menu__body');
+let in_header = document.querySelector('.contacts-header');
+
 let from_actions = document.querySelector('._actions');
-let from_info = document.querySelector('._info');
+let from_info = document.querySelector('.info-header');
+
 let actions = document.querySelector('.actions-header');
-let info = document.querySelector('.info-header');
+let info_callback = document.querySelector('._callback');
+let info_schedule = document.querySelector('._schedule');
+let info_cart = document.querySelector('._cart');
 
 window.addEventListener('resize', function (event) {
     let viewport_width = Math.max(widthContenArea1 = window.innerWidth || document.documentElement.clientWidth ||
         document.body.clientWidth);
+
+    if (viewport_width < 992) {
+        if (!info_schedule.classList.contains('replaced')) {
+            in_burger.append(info_schedule);
+            info_schedule.classList.add('replaced');
+        }
+    }
+    else {
+        from_info.prepend(info_schedule);
+        info_schedule.classList.remove('replaced');
+    }
+
     if (viewport_width < 640) {
-        if (!actions.classList.contains('done') && !info.classList.contains('done')) {
+        if (!actions.classList.contains('replaced') && !info_callback.classList.contains('replaced') && !info_cart.classList.contains('replaced')) {
             in_burger.prepend(actions);
-            in_burger.append(info);
-            actions.classList.add('done');
-            info.classList.add('done');
+            in_burger.append(info_callback);
+            in_header.prepend(info_cart);
+            actions.classList.add('replaced');
+            info_callback.classList.add('replaced');
+            info_cart.classList.add('replaced');
         }
     }
     else {
         from_actions.append(actions);
-        from_info.append(info);
-        actions.classList.remove('done');
-        info.classList.remove('done');
+        from_info.prepend(info_callback);
+        from_info.append(info_cart);
+        actions.classList.remove('replaced');
+        info_callback.classList.remove('replaced');
+        info_cart.classList.remove('replaced');
     }
 })
